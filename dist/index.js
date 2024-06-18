3ca698975e44d283c3aaa3ac2dd1f0e6cfa05273
Juan José Vázquez Gil <juanjose.vazquez@prefapp.es>
2024-06-18 15:01:34 +0200

    feat: Compiled latest version

--- dist/index.js
+++ dist/index.js
@@ -34580,7 +34580,9 @@ async function run() {
               stateRepo.registry || defaultRegistries[dispatch.type]
             const imageRepo = `${registry}/${originRepo}`
 
-            console.log(`github.context.repo: ${github.context.repo}`)
+            console.log(
+              `github.context.repo: ${github.context.repo.owner},${github.context.repo.repo}`
+            )
             console.log('Registry debug')
             console.log(`stateRepo.registry: ${stateRepo.registry}`)
             console.log(`defaultRegistry: ${defaultRegistries[dispatch.type]}`)

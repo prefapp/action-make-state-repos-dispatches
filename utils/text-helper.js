67a51918d0997941c8dfc3919d4dac566e74d4ca
Juan José Vázquez Gil <juanjose.vazquez@prefapp.es>
2024-10-08 13:47:58 +0200

    fix: Added clearer errors

--- utils/text-helper.js
+++ utils/text-helper.js
@@ -1,11 +1,15 @@
 const YAML = require('yaml')
 
 function parseFile(fileContent, encoding = '') {
-  if (encoding) {
-    fileContent = Buffer.from(fileContent, encoding).toString('utf-8')
-  }
+  try {
+    if (encoding) {
+      fileContent = Buffer.from(fileContent, encoding).toString('utf-8')
+    }
 
-  return YAML.parse(fileContent, 'utf8')
+    return YAML.parse(fileContent, 'utf8')
+  } catch (err) {
+    throw new Error(`Error parsing YAML file: {err.message}`)
+  }
 }
 
 module.exports = {

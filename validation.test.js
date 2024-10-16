905daa8c15bccb66a95df4cffab1e241d2de6439
JulianGV21 <llulian.gomez@gmail.com>
2024-10-16 14:08:34 +0200

    Updated jsonschema and validations test

---
+++ validation.test.js
@@ -0,0 +1,38 @@
+const fs = require('fs')
+const path = require('path')
+const yaml = require('js-yaml')
+const Ajv = require('ajv')
+
+describe('Yaml validation against Json schema', () => {
+  let yamlData;
+  let schema;
+  let validate;
+
+  beforeAll (() => {
+    const yamlFilePath = path.join(__dirname, '/fixtures/github/make_dispatches.yaml');
+    const yamlConten = fs.readFileSync(yamlFilePath, 'utf8');
+    yamlData = yaml.load(yamlConten);
+
+    const schemaFilePath = path.join(__dirname, '/schema/jsonschema.json');
+    schema = JSON.parse(fs.readFileSync(schemaFilePath, 'utf8'));
+
+    const ajv = new Ajv();
+    validate = ajv.compile(schema);
+});
+
+  test('should validate Yaml successfully against the Json Schema', () =>{
+    const valid = validate(yamlData);
+    if (!valid) {
+      console.error('Validation errors:', validate.errors);
+    }
+    expect(valid).toBe(true);
+});
+
+  test('should fail if Yaml data does not match the schema', () => {
+    const invalidYamlData = {...yamlData, extraField: 'invalid'};
+
+    const valid = validate(invalidYamlData);
+    expect(valid).toBe(false);
+    expect(validate.errors).toBeDefined();
+  });
+});

31ea5eef0e9f88e404707f32cbd399ccc4c3ede6
JulianGV21 <llulian.gomez@gmail.com>
2024-10-17 11:44:00 +0200

    Fix conflicts

--- validation.test.js
+++ validation.test.js
@@ -42,13 +42,13 @@ describe('Yaml validation against Json schema', () => {
   test('should fail if a required field is missing in Yaml', () => {
     const yamlWithoutRequiredField = {
       ...yamlData,
-      dispatches: yamlData.dispatches.map(dispatch =>({
+      dispatches: yamlData.dispatches.map(dispatch => ({
         ...dispatch,
-        state_repos: dispatch.state_repos.map(repo =>{
-          const { repo: removedRepo, ...rest} = repo;
+        state_repos: dispatch.state_repos.map(repo => {
+          const { repo: removedRepo, ...rest } = repo
         })
       }))
-    };
+    }
 
     const valid = validate(yamlWithoutRequiredField)
     expect(valid).toBe(false)

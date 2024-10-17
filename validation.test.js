543ab6e3151a23a58f9b09ebf4a1ecc9255172b4
JulianGV21 <llulian.gomez@gmail.com>
2024-10-17 11:31:36 +0200

    Chage in code writing of validation.test

--- validation.test.js
+++ validation.test.js
@@ -4,43 +4,49 @@ const yaml = require('yaml')
 const Ajv = require('ajv')
 
 describe('Yaml validation against Json schema', () => {
-  let yamlData;
-  let schema;
-  let validate;
+  let yamlData
+  let schema
+  let validate
 
-  beforeAll (() => {
-    const yamlFilePath = path.join(__dirname, '/fixtures/github/make_dispatches.yaml');
-    const yamlConten = fs.readFileSync(yamlFilePath, 'utf8');
-    yamlData = yaml.parse(yamlConten);
+  beforeAll(() => {
+    const yamlFilePath = path.join(
+      __dirname,
+      '/fixtures/github/make_dispatches.yaml'
+    )
+    const yamlConten = fs.readFileSync(yamlFilePath, 'utf8')
+    yamlData = yaml.parse(yamlConten)
 
-    const schemaFilePath = path.join(__dirname, '/schema/jsonschema.json');
-    schema = JSON.parse(fs.readFileSync(schemaFilePath, 'utf8'));
+    const schemaFilePath = path.join(__dirname, '/schema/jsonschema.json')
+    schema = JSON.parse(fs.readFileSync(schemaFilePath, 'utf8'))
 
-    const ajv = new Ajv();
-    validate = ajv.compile(schema);
-});
+    const ajv = new Ajv()
+    validate = ajv.compile(schema)
+  })
 
-  test('should validate Yaml successfully against the Json Schema', () =>{
-    const valid = validate(yamlData);
+  test('should validate Yaml successfully against the Json Schema', () => {
+    const valid = validate(yamlData)
     if (!valid) {
-      console.error('Validation errors:', validate.errors);
+      console.error('Validation errors:', validate.errors)
     }
-    expect(valid).toBe(true);
-});
+    expect(valid).toBe(true)
+  })
 
   test('should fail if Yaml data does not match the schema', () => {
-    const invalidYamlData = {...yamlData, extraField: 'invalid'} ;
+    const invalidYamlData = { ...yamlData, extraField: 'invalid' }
 
-    const valid = validate(invalidYamlData);
-    expect(valid).toBe(false);
-    expect(validate.errors).toBeDefined();
-  });
+    const valid = validate(invalidYamlData)
+    expect(valid).toBe(false)
+    expect(validate.errors).toBeDefined()
+  })
 
-  test ('should fail if a required field is missing in Yaml', () => {
-    const yamlWithoutRequiredField = { requiredField, ...yamlWithoutRequiredField };
+  test('should fail if a required field is missing in Yaml', () => {
+    const yamlWithoutRequiredField = {
+      requiredField,
+      ...yamlWithoutRequiredField
+    }
 
-    const valid = validate(yamlWithoutRequiredField);
-    expect(valid).toBe(false);
-    expect(validate.errors).toBeDefined();
-  });
-});
+    const valid = validate(yamlWithoutRequiredField)
+    expect(valid).toBe(false)
+    expect(validate.errors).toBeDefined()
+  })
+})

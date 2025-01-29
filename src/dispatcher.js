84572654f103d312c2261996d3785385867f2b81
Juan José Vázquez Gil <juanjose.vazquez@prefapp.es>
2025-01-29 13:22:57 +0100

    test: Improved logging temporaly

--- src/dispatcher.js
+++ src/dispatcher.js
@@ -221,7 +221,9 @@ function createDispatchList(
 ) {
   const dispatchList = []
 
+  debug('Creating dispatch list')
   for (const deployment of deployments) {
+    debug('Validating deployment >', deployment)
     const chosenCluster = deployment.platform
 
     if (!clusterConfig[chosenCluster].tenants.includes(deployment.tenant)) {
